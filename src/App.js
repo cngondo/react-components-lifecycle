e421f96ebe892c3b77401254bd93c535f5cf8d2b
cngondo <devngondo@gmail.com>
2019-03-28 01:27:57 -0600

    React components lifecycle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  // All react lifecycle methods
+
+  // 1. Constructor
+  // Function: Setting state, creating refs and method binding
+  constructor(props) {
+    super(props);
+    // some random state
+    this.state = {
+      counter: 0
+    };
+    console.log("Constructor loaded");
+
+    // Bind your custom methods here
+    this.addCounter = this.addCounter.bind(this);
+  }
+
+  //2. getDerivedStateFromProps
+  //Function: Returning state based on initial props
+  static getDerivedStateFromProps(props, state) {
+    console.log("getDerivedStateFromProps");
+    return { state };
+  }
+
+  //3. render method gets called on the third step and we now have a component
+  //to work with
+
+  //4. componentDidMount
+  //Function: Make AJAX call to load data for your request
+  componentDidMount() {
+    console.log("Component was successfully mounted");
+  }
+
+  //5. getDerivedStateFromProps
+  //Function: Updating state based on props if props keep changing
+
+  //6. shouldComponentUpdate
+  //Function: Controlling when your component will rerender
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(`These are the nextProps ${nextProps} 
+    This is the nextState ${nextState}`);
+    return true;
+  }
+
+  //7. Render gets called again
+
+  //8. getSnapshotBeforeUpdate
+  //Function: Passing DOM attributes to componentDidUpdate
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    return null;
+  }
+
+  //9. componentDidUpdate
+  //Function: Performs any form of action after DOM has been updated
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(`These are the prevProps ${prevProps} 
+    This is the prevState ${prevState}`);
+    console.log("Component Updated");
+  }
+
+  //10. componentWillUnmount
+  //Function: clear timers, intervals, close DB connections
+  componentWillUnmount() {
+    console.log("Component is unmounting");
+  }
+
+  // Response to error events on child components
+  // getDerivedStateFromError
+  //Function: Updates state to display errors from child components
+  static getDerivedStateFromError(error) {
+    console.error(error);
+  }
+
+  // componentDidCatch
+  // Functions: Logging errors
+  componentDidCatch(error, info) {
+    console.log("Whoops, error in one of the lifecycle methods!");
+  }
+
+  addCounter() {
+    let { counter } = this.state;
+    counter += 1;
+    this.setState({ counter });
+  }
+
   render() {
+    const { counter } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        Some counter component
+        <br />
+        <button onClick={this.addCounter}>Add Counter</button>
+        {counter}
       </div>
     );
   }
